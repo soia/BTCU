d7bef19441a7a52c8d043b071b5a798660325915
Nikita Soia <nikita.soya@gmail.com>
2020-10-08 17:31:51 +0300

    added footer

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,6 +4,7 @@ import ReactNotification from 'react-notifications-component';
 import ScrollToTop from '../../helpers/scroll-to-top';
 import { PageNotFound } from '../pages';
 import HomePage from '../pages/home-page/home-page';
+import Footer from '../layouts/footer';
 import '../assets/styles/reset.scss';
 import './app.scss';
 import '../assets/styles/fonts.scss';
@@ -11,12 +12,12 @@ import 'react-notifications-component/dist/theme.css';
 
 const App = () => (
     <Router>
-
         <ScrollToTop>
             <Switch>
                 <Route path="/" component={HomePage} exact />
                 <Route component={PageNotFound} />
             </Switch>
+            <Footer />
         </ScrollToTop>
         <ReactNotification />
     </Router>
--- src/components/layouts/footer/footer.jsx
+++ src/components/layouts/footer/footer.jsx
@@ -1,54 +1,67 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import DocIcon from '../../assets/images/footer-icons/doc-icon';
-import whitepaper from '../../assets/documents/white_paper_etalonium.pdf';
-import termsAndConditions from '../../assets/documents/t_c_etalonium.pdf';
-import WebSiteIcon from '../../assets/images/footer-icons/web-site_icon';
+import logo from '../../assets/images/logos/btcu.svg';
+import companyIcon from '../../assets/images/icons/company-icon.svg';
+import locationIcon from '../../assets/images/icons/location-icon.svg';
+import contactIcon from '../../assets/images/icons/contact-icon.svg';
 import style from './footer.module.scss';
 
-const Footer = ({ className }) => {
+const Footer = () => {
     const { t } = useTranslation();
 
     return (
-        <footer className={className} id="footer">
-            <a
-                href={whitepaper}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={style.footer_item}
-            >
-                <DocIcon className={style.footer_img} />
-                Whitepaper
-            </a>
-            <a
-                href={termsAndConditions}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={style.footer_item}
-            >
-                <DocIcon className={style.footer_img} />
-                {t('termsAndConditions')}
-            </a>
-            <a
-                href="https://www.etalonium.io/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={style.footer_item}
-            >
-                <WebSiteIcon className={style.footer_img} />
-                {t('webSite')}
-            </a>
+        <footer className={style.footer} id="footer">
+            <div className={style.footer__top}>
+                <Link to="/" className={style.footer__logo}>
+                    <img className={style.footer__logo_img} src={logo} alt="logo" />
+                </Link>
+                <div className={style.linksWrapper}>
+                    <ul className={style.list}>
+                        <li className={style.list__title}>
+                            <img src={companyIcon} alt="companyIcon" />
+                            {t('company')}
+                        </li>
+                        <li className={style.list__link}>
+                            <Link to="/">{t('aboutUs')}</Link>
+                        </li>
+                        <li className={style.list__link}>
+                            <Link to="/">{t('advantages')}</Link>
+                        </li>
+                        <li className={style.list__link}>
+                            <Link to="/">{t('airdrop')}</Link>
+                        </li>
+                        <li className={style.list__link}>
+                            <Link to="/">{t('partners')}</Link>
+                        </li>
+                    </ul>
+                    <ul className={style.list}>
+                        <li className={style.list__title}>
+                            <img src={locationIcon} alt="location" />
+                            {t('location')}
+                        </li>
+                        <li className={style.list__text}>{t('hongKongChina')}</li>
+                        <li className={style.list__text}>7/F MW Tower 111</li>
+                        <li className={style.list__text}>Bonham Strand</li>
+                        <li className={style.list__text}>Sheung Wan</li>
+                    </ul>
+                    <ul className={style.list}>
+                        <li className={style.list__title}>
+                            <img src={contactIcon} alt="contact" />
+                            {t('contact')}
+                        </li>
+                        <li className={style.list__link}>
+                            <a href="mailto:support@btcu.io">support@btcu.io</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className={style.footer__bottom}>
+                <p>Prof-IT Blockchain Limited</p>
+                <p>{t('allRightsReserved')}, Bitcoin Ultimatum</p>
+            </div>
         </footer>
     );
-};
-
-Footer.defaultProps = {
-    className: '',
-};
-
-Footer.propTypes = {
-    className: PropTypes.string,
 };
 
 export default Footer;
