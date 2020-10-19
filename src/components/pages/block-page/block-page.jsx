ddb550a7b98f0865618bd1190383eca66e722a39
Nikita Soia <nikita.soya@gmail.com>
2020-10-19 15:33:04 +0300

    start block page

--- src/components/pages/block-page/block-page.jsx
+++ src/components/pages/block-page/block-page.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { compose } from '../../../utils';
 import style from './block-page.module.scss';
@@ -21,7 +21,11 @@ class BlockPage extends Component {
     };
 
     componentDidMount() {
-        const { match: { params: { id } } } = this.props;
+        const {
+            match: {
+                params: { id },
+            },
+        } = this.props;
 
         this.setState({
             block: id,
@@ -35,8 +39,39 @@ class BlockPage extends Component {
         return (
             <div className={style.block}>
                 <h1 className={style.block__title}>
-                    {t('BTCU block height')}{' '}#{block}
+                    BTCU{t(' blockHeight')} #{block}
                 </h1>
+                <div className={style.block__top}>
+                    <Link to="/" className={style.block__top_address}>
+                        435483f302b54d553fa265364137878214c019be83940a5187a631cc8880df2a
+                    </Link>
+                    <div className={style.block__top_wrapper}>
+                        <p className={style.block__top_date}>
+                            2020-10-05 13:01:45 {t('extractedProofOfStake')}
+                        </p>
+                        <p className={style.block__top_transactions}>
+                            {t('transactions')}: 4 <span>0.8 kB</span>
+                        </p>
+                    </div>
+                </div>
+                <div className={style.block__bottom}>
+                    <div className={style.block__bottom_item}>
+                        <p className={style.block__bottom_label}>{t('valueOut')}</p>
+                        <p className={style.block__bottom_value}>528.79732796 BTCU</p>
+                    </div>
+                    <div className={style.block__bottom_item}>
+                        <p className={style.block__bottom_label}>{t('difficulty')}</p>
+                        <p className={style.block__bottom_value}>10776.3455936409</p>
+                    </div>
+                    <div className={style.block__bottom_item}>
+                        <p className={style.block__bottom_label}>{t('outstanding')}</p>
+                        <p className={style.block__bottom_value}>-67,421,881.343 BTCU</p>
+                    </div>
+                    <div className={style.block__bottom_item}>
+                        <p className={style.block__bottom_label}>{t('created')}</p>
+                        <p className={style.block__bottom_value}>4.99957782 BTCU</p>
+                    </div>
+                </div>
             </div>
         );
     }
